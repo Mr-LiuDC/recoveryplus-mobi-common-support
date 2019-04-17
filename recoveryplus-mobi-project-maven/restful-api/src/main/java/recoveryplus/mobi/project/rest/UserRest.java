f18186932c0b3627a75bacc916a13dffa636b361
liudecai <liudecai@postop.cn>
2019-04-18 07:22:25 +0800

    [Add]Add UserRest.java

---
+++ recoveryplus-mobi-project-maven/restful-api/src/main/java/recoveryplus/mobi/project/rest/UserRest.java
@@ -0,0 +1,17 @@
+package recoveryplus.mobi.project.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户Rest接口
+ *
+ * @author LiuDecai
+ */
+@RestController
+@RequestMapping("/api/user")
+public class UserRest {
+
+
+
+}
