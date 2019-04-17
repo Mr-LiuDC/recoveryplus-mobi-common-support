4ba8382d7d065a3602871c8ce905bfb8050a4366
刘德财 <liudecai@postop.cn>
2019-04-17 20:16:06 +0800

    [Add]Add project recoveryplus-mobi-project-maven

---
+++ recoveryplus-mobi-project-maven/restful-api/src/main/java/recoveryplus/mobi/project/RuneApplication.java
@@ -0,0 +1,18 @@
+package recoveryplus.mobi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用启动类
+ *
+ * @author LiuDecai
+ */
+@SpringBootApplication
+public class RuneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuneApplication.class, args);
+    }
+
+}
---
+++ recoveryplus-mobi-project-maven/restful-api/src/test/java/recoveryplus/mobi/project/RuneApplicationTest.java
@@ -0,0 +1,22 @@
+package recoveryplus.mobi.project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 应用启动测试
+ *
+ * @author LiuDecai
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RuneApplicationTest {
+
+    @Test
+    public void loadContext() {
+
+    }
+
+}
