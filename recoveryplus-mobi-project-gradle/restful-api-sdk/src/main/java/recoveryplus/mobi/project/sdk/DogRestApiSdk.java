cd19249b01c9a0c4e06489ae75bb28b14bec5d0e
刘德财 <liudecai@postop.cn>
2019-04-19 18:45:55 +0800

    [Change]完善recoveryplus-mobi-project-gradle

---
+++ recoveryplus-mobi-project-gradle/restful-api-sdk/src/main/java/recoveryplus/mobi/project/sdk/DogRestApiSdk.java
@@ -0,0 +1,14 @@
+package recoveryplus.mobi.project.sdk;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * Dog Rest Api SDK
+ *
+ * @author LiuDecai
+ */
+@FeignClient
+public interface DogRestApiSdk {
+
+
+}
